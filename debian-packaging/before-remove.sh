cde80abc8807c5204f5e979bf3d8f591c26a0b0c
Zundra Daniel <zundra.daniel@gmail.com>
2016-04-16 20:47:14 +0000

    created a before-remove script

---
+++ debian-packaging/before-remove.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+/sbin/stop tdfw > /dev/null 2>&1 || /bin/true
+
+/bin/echo "Removing portal nginx config.."
+/bin/rm -rf /etc/nginx/sites-enabled/tdfw || /bin/true
+/bin/rm -rf /etc/nginx/sites-available/tdfw  || /bin/true
+/usr/sbin/service nginx reload || /bin/true
+
